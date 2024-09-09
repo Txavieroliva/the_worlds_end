9eaecfc78d07247afe48b98f7a2642e1eb83fbb7
Thiago Xavier <xavieroliva.thiagoe@gmail.com>
2024-09-09 17:36:37 -0300

    Añadido movimiento basico

--- Assets/Scripts/Player/Earth_Elemental/PlayerMovement.cs
+++ Assets/Scripts/Player/Earth_Elemental/PlayerMovement.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float speed = 6.0f;
+    private Rigidbody rb;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //movimiento direccional
+        float movimientoHorizontal = Input.GetAxis("Horizontal");
+        float movimientoVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(movimientoHorizontal, 0.0f, movimientoVertical);
+        rb.AddForce(movement * speed);
+
+
     }
 }
