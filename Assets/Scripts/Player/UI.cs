8db43e5f12e6c419b98d21f3094c5c4a28dcc806
Thiago Xavier <xavieroliva.thiagoe@gmail.com>
2024-09-12 17:55:49 -0300

    Se añade barra de mana y su función
    Se añade una barra de mana que se consumirá al hacer click izquierdo con el raton. Gastandola y llevandola a 0

--- Assets/Scripts/Player/UI.cs
+++ Assets/Scripts/Player/UI.cs
@@ -4,24 +4,32 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class healthBar : MonoBehaviour
+public class UI : MonoBehaviour
 {
-   public Slider healthSlider;
-   public Slider transHealthSlider;
+   [SerializeField] Slider healthSlider;
+   [SerializeField] Slider transHealthSlider;
+   [SerializeField] Slider manaSlider;
+   [SerializeField] Slider transManaSlider;
    public float maxHealth = 100f;
    public float health;
+   public float maxMana = 100f;
+   public float mana;
    private float lerpSpeed = 0.005f;
 
    private void Start()
    {
         health = maxHealth;
         transHealthSlider.value = maxHealth;
+        mana = maxMana;
+        transManaSlider.value = maxMana;
    } 
 
    private void Update()
    {
         updateHealth();
         healthTransition();
+        manaUpdate();
+        manaTransition();
    }
 
    private void updateHealth()
@@ -44,6 +52,32 @@ public class healthBar : MonoBehaviour
         {
           transHealthSlider.value = Mathf.Lerp(transHealthSlider.value, health, lerpSpeed);
         }
+   }
+
+   private void manaUpdate()
+   {
+     if(manaSlider.value != mana)
+     {
+          manaSlider.value = mana;
+     }
+
+     if(Input.GetKeyDown(KeyCode.Mouse0))
+     {
+          useMana(15);
+     }
+   }
+
+   private void manaTransition()
+   {
+     if(manaSlider.value != transManaSlider.value)
+     {
+          transManaSlider.value = Mathf.Lerp(transManaSlider.value, mana, lerpSpeed);
+     }
+   }
+
+   private void useMana(float energy)
+   {
+     mana -= energy;
    }
 
    private void takeDamage(float damage)
