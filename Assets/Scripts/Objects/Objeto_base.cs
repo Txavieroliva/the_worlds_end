ffa2f6094c6754f02ccd4120d5edbb1d040b1003
Agustin-GD <agustinvieja000@gmail.com>
2024-09-23 15:44:43 -0300

    Inicio del objeto base
    Copiado y pegado todo lo realizado en el excel.

---
+++ Assets/Scripts/Objects/Objeto_base.cs
@@ -0,0 +1,105 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Objeto_base : MonoBehaviour
+{
+protected string Identificador;
+protected string Nombre;
+public string Lugar_de_pertenencia;
+protected string Idle;
+protected string Material;
+protected int Firmeza_al_suelo;
+protected int Masa;
+protected int Masa_aguantada;
+protected int Velocidad;
+public int Vida;
+protected int Threshold_daño;
+//Vida_anual
+public bool Agarrable;
+protected bool Diferente_noche;
+protected bool Diferente_climas;
+protected bool Crece;
+public bool Destructible;
+    
+    
+    // Start is called before the first frame update
+void Start()
+    {
+        
+    }
+
+protected void	Utilizar_nombre(Nombre)
+    {
+
+    }
+public void	Settear_spawner(Lugar)
+    {
+
+    }
+protected string	Idles()
+    {
+
+    }
+public void	Interaccion()
+    {
+
+    }
+protected string	Destruccion_completa()
+    {
+
+    }
+protected void	Golpeado()
+    {
+
+    }
+protected void	Destruccion_porcentual()
+    {
+
+    }
+protected void	Detector_daño()
+    {
+
+    }
+protected void	Calculadora_daño()
+    {
+
+    }
+protected void	Ser_agarrado()
+    {
+
+    }
+protected void	Animator()
+    {
+
+    }
+protected void	Optimizar()
+    {
+
+    }
+protected void	Informacion_guardar()
+    {
+
+    }
+protected void	Informacion_cargar()
+    {
+
+    }
+protected void	Algo_encima()
+    {
+
+    }
+protected void	Crecer()
+    {
+
+    }
+    
+    
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
